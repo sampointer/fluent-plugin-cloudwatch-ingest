3a3f3fa71c87229a0cb47358a76a3e24578105b7
sam <Sam Pointer>
2017-03-09 15:08:27 +0000

    bundle gem fluent-plugin-cloudwatch-ingest/ --no-bin -t

---
+++ lib/fluent/plugin/cloudwatch/ingest.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/cloudwatch/ingest/version"
+
+module Fluent
+  module Plugin
+    module Cloudwatch
+      module Ingest
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/fluent/plugin/cloudwatch/ingest_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Fluent::Plugin::Cloudwatch::Ingest do
+  it 'has a version number' do
+    expect(Fluent::Plugin::Cloudwatch::Ingest::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
