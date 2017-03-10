f7e9f3ba9b5732b5e2d32ec80d91ed4e2c8ee0ed
sam <Sam Pointer>
2017-03-10 18:54:48 +0000

    autocorrect whitespace

--- lib/fluent/plugin/cloudwatch/ingest.rb
+++ lib/fluent/plugin/cloudwatch/ingest.rb
@@ -1,9 +1,9 @@
-module Fluent                                                                                                                                                             
-  module Plugin                                                                                                                                                           
-    module Cloudwatch                                                                                                                                                     
-      module Ingest                                                                                                                                                       
-        # Your code goes here...                                                                                                                                          
-      end                                                                                                                                                                 
-    end                                                                                                                                                                   
-  end                                                                                                                                                                     
+module Fluent
+  module Plugin
+    module Cloudwatch
+      module Ingest
+        # Your code goes here...
+      end
+    end
+  end
 end
