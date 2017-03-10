fe6b1509ea33c6eb9c7b64267a2e7062967b3ac2
sam <Sam Pointer>
2017-03-10 12:47:08 +0000

    restore stock version for a gemspec easy life

---
+++ lib/fluent/plugin/cloudwatch/ingest/version.rb
@@ -0,0 +1,9 @@
+module Fluent                                                                                                                                                             
+  module Plugin                                                                                                                                                           
+    module Cloudwatch                                                                                                                                                     
+      module Ingest                                                                                                                                                       
+        VERSION = "0.1.0"                                                                                                                                                 
+      end                                                                                                                                                                 
+    end                                                                                                                                                                   
+  end                                                                                                                                                                     
+end
