61785f9afc2882e764c6836a624e031181c74be2
sam <Sam Pointer>
2017-03-10 13:13:05 +0000

    autocorrect some more offenses

--- lib/fluent/plugin/in_cloudwatch.rb
+++ lib/fluent/plugin/in_cloudwatch.rb
@@ -7,7 +7,6 @@ require 'yaml'
 
 module Fluent::Plugin
   class Cloudwatch < Input
-
     Fluent::Plugin.register_input('cloudwatch', self)
     helpers :parser, :compat_parameters
 
@@ -20,9 +19,9 @@ module Fluent::Plugin
     desc 'The session name for use with STS'
     config_param :sts_session_name, :string, default: 'fluentd'
     desc 'Log group name or prefix. Not setting means "all"'
-    config_param :log_group_name_prefix, :string, default: ""
+    config_param :log_group_name_prefix, :string, default: ''
     desc 'Log stream name or prefix. Not setting means "all"'
-    config_param :log_stream_name_prefix, :string, default: ""
+    config_param :log_stream_name_prefix, :string, default: ''
     desc 'State file name'
     config_param :state_file_name, :string, default: '/var/spool/td-agent/cloudwatch.state'
     desc 'Fetch logs every interval'
@@ -40,17 +39,17 @@ module Fluent::Plugin
 
   def start
     # Get a handle to Cloudwatch
-    aws_options = Hash.new
+    aws_options = {}
     Aws.config[:region] = @region
     if @sts_enabled
       aws_options[:credentials] = Aws::AssumeRoleCredentials.new(
-              role_arn: @sts_arn,
-              role_session_name: @sts_session_name
-        )
+        role_arn: @sts_arn,
+        role_session_name: @sts_session_name
+      )
 
       log.info("Using STS for authentication with source account ARN: #{@sts_arn}, session name: #{@sts_session_name}")
     else
-      log.info("Using local instance IAM role for authentication")
+      log.info('Using local instance IAM role for authentication')
     end
     @aws = Aws::CloudWatchLogs::Client.new(aws_options)
     @finished = false
@@ -63,6 +62,7 @@ module Fluent::Plugin
   end
 
   private
+
   def configure_parser(conf)
     if conf[:format]
       @parser = Fluent::TextParser.new
@@ -76,14 +76,14 @@ module Fluent::Plugin
 
   def run
     until @finished
-      log_groups = Array.new
+      log_groups = []
       state = State.new(@state_file_name)
-      
+
       # Fetch all log group names
       next_token = nil
-      while true do
+      loop do
         begin
-          response = @aws.describe_log_groups({log_group_name_prefix: @log_group_name_prefix, next_token: next_token})
+          response = @aws.describe_log_groups(log_group_name_prefix: @log_group_name_prefix, next_token: next_token)
           response.log_groups.each { |g| log_groups << g.log_group_name }
           break unless response.next_token
           next_token = response.next_token
@@ -95,11 +95,11 @@ module Fluent::Plugin
 
       # Fetch the streams for each log group
       log_groups.each do |group|
-        log_streams = Array.new
+        log_streams = []
         next_token = nil
-        while true do
+        loop do
           begin
-            response = @aws.describe_log_streams({log_group_name: group, log_stream_name_prefix: @log_stream_name_prefix, next_token: next_token})
+            response = @aws.describe_log_streams(log_group_name: group, log_stream_name_prefix: @log_stream_name_prefix, next_token: next_token)
             response.log_streams.each { |s| log_streams << s.log_stream_name }
             break unless reponse.next_token
             next_token = reponse.next_token
@@ -113,15 +113,11 @@ module Fluent::Plugin
           # See if we have some stored state for this group and stream.
           # If we have then use the stored forward_token to pick up
           # from that point. Otherwise start from the start.
-          if state[group][stream]
-            stream_token = state[group][stream]
-          else
-            stream_token = nil
-          end
+          stream_token = (state[group][stream] if state[group][stream])
 
           begin
-            while true do
-              response = @aws.get_log_events({log_group_name: group, log_stream_name: stream, next_token: stream_token})
+            loop do
+              response = @aws.get_log_events(log_group_name: group, log_stream_name: stream, next_token: stream_token)
               emit(response.events)
               break unless response.next_token
               stream_token = response.next_token
@@ -135,7 +131,7 @@ module Fluent::Plugin
         end
       end
 
-      log.info("Pruning and saving state")
+      log.info('Pruning and saving state')
       state.prune(log_groups) # Remove any log groups and associated streams that no longer exist
       state.save
       log.info("Pausing for #{@interval}")
@@ -148,41 +144,38 @@ module Fluent::Plugin
 
     def initialize(filepath)
       self.statefile = Pathname.new(filepath)
-      unless self.statefile.exists?
-        log.warn("State file #{self.statefile} does not exist. Creating a new one.")
+      unless statefile.exists?
+        log.warn("State file #{statefile} does not exist. Creating a new one.")
         begin
-          self.save
+          save
         rescue => boom
-          log.error("Unable to create new state file #{self.statefile}: #{boom}")
+          log.error("Unable to create new state file #{statefile}: #{boom}")
         end
       end
 
       begin
-        self.merge!(YAML.load(self.statefile.read))
-        log.info("Loaded state for #{self.keys.size} log groups from #{self.statefile}")
+        merge!(YAML.safe_load(statefile.read))
+        log.info("Loaded state for #{keys.size} log groups from #{statefile}")
       rescue => boom
         log.error("Unable to read state file #{statefile}: #{boom}")
       end
     end
 
     def save
-      begin
-        file = File.open(self.statefile, 'w')
-        file.write(self)
-        file.close
-        log.info("Saved state to #{self.statefile}")
-      rescue => boom
-        log.error("Unable to write state file #{self.statefile}: #{boom}")
-      end
+      file = File.open(statefile, 'w')
+      file.write(self)
+      file.close
+      log.info("Saved state to #{statefile}")
+    rescue => boom
+      log.error("Unable to write state file #{statefile}: #{boom}")
     end
 
     def prune(log_groups)
-      groups_before = self.keys.size
-      self.delete_if { |k,v| true unless log_groups.key?(k) }
-      log.info("Pruned #{before - self.keys.size} keys from state file")
+      groups_before = keys.size
+      delete_if { |k, _v| true unless log_groups.key?(k) }
+      log.info("Pruned #{before - keys.size} keys from state file")
 
       # TODO: also prune streams as these are most likely to be transient
     end
   end
-
 end
