28eeadda78d2ef920bb39600f40d35f72cb63794
sam <Sam Pointer>
2017-03-10 18:18:12 +0000

    re-add missing boilerplate

---
+++ lib/fluent/plugin/cloudwatch/ingest.rb
@@ -0,0 +1,9 @@
+module Fluent                                                                                                                                                             
+  module Plugin                                                                                                                                                           
+    module Cloudwatch                                                                                                                                                     
+      module Ingest                                                                                                                                                       
+        # Your code goes here...                                                                                                                                          
+      end                                                                                                                                                                 
+    end                                                                                                                                                                   
+  end                                                                                                                                                                     
+end
