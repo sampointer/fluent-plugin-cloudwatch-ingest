cdca3b75cfa3d44c0c9409dfaaf826d5e8ba3f20
sam <Sam Pointer>
2017-03-10 13:50:08 +0000

    correct final outstanding rubocop offenses

--- lib/fluent/plugin/in_cloudwatch.rb
+++ lib/fluent/plugin/in_cloudwatch.rb
@@ -23,7 +23,7 @@ module Fluent::Plugin
     desc 'Log stream name or prefix. Not setting means "all"'
     config_param :log_stream_name_prefix, :string, default: ''
     desc 'State file name'
-    config_param :state_file_name, :string, default: '/var/spool/td-agent/cloudwatch.state'
+    config_param :state_file_name, :string, default: '/var/spool/td-agent/cloudwatch.state' # rubocop:disable all
     desc 'Fetch logs every interval'
     config_param :interval, :time, default: 60
   end
@@ -65,7 +65,7 @@ module Fluent::Plugin
   private
 
   def configure_parser(conf)
-    if conf[:format]
+    if conf[:format] # rubocop:disable all
       @parser = Fluent::TextParser.new
       @parser.configure(conf)
     end
@@ -75,42 +75,62 @@ module Fluent::Plugin
     # TODO: I need to do something useful
   end
 
+  def log_groups(log_group_prefix)
+    log_groups = []
+
+    # Fetch all log group names
+    next_token = nil
+    loop do
+      begin
+        response = @aws.describe_log_groups(
+          log_group_name_prefix: log_group_prefix,
+          next_token: next_token
+        )
+
+        response.log_groups.each { |g| log_groups << g.log_group_name }
+        break unless response.next_token
+        next_token = response.next_token
+      rescue => boom
+        log.error("Unable to retrieve log groups: #{boom}")
+      end
+    end
+    log.info("Found #{log_groups.size} log groups")
+
+    return log_groups
+  end
+
+  def log_streams(log_group_name, log_stream_name_prefix)
+    log_streams = []
+    next_token = nil
+    loop do
+      begin
+        response = @aws.describe_log_streams(
+          log_group_name: group,
+          log_stream_name_prefix: log_stream_name_prefix,
+          next_token: next_token
+        )
+
+        response.log_streams.each { |s| log_streams << s.log_stream_name }
+        break unless reponse.next_token
+        next_token = reponse.next_token
+      rescue => boom
+        log.error("Unable to retrieve log streams for group #{group}
+          with stream prefix #{log_stream_name_prefix}: #{boom}")
+      end
+    end
+    log.info("Found #{log_streams.size} streams for #{log_group_name}")
+
+    return log_streams
+  end
+
   def run
     until @finished
-      log_groups = []
       state = State.new(@state_file_name)
 
-      # Fetch all log group names
-      next_token = nil
-      loop do
-        begin
-          response = @aws.describe_log_groups(log_group_name_prefix: @log_group_name_prefix, next_token: next_token)
-          response.log_groups.each { |g| log_groups << g.log_group_name }
-          break unless response.next_token
-          next_token = response.next_token
-        rescue => boom
-          log.error("Unable to retrieve log groups: #{boom}")
-        end
-      end
-      log.info("Found #{log_groups.size} log groups")
-
       # Fetch the streams for each log group
-      log_groups.each do |group|
-        log_streams = []
-        next_token = nil
-        loop do
-          begin
-            response = @aws.describe_log_streams(log_group_name: group, log_stream_name_prefix: @log_stream_name_prefix, next_token: next_token)
-            response.log_streams.each { |s| log_streams << s.log_stream_name }
-            break unless reponse.next_token
-            next_token = reponse.next_token
-          rescue => boom
-            log.error("Unable to retrieve log streams for group #{group} with stream prefix #{@log_stream_name_prefix}: #{boom}")
-          end
-        end
-
+      log_groups(@log_group_name_prefix).each do |group|
         # For each log stream get and emit the events
-        log_streams.each do |stream|
+        log_streams(group, @log_stream_name_prefix).each do |stream|
           # See if we have some stored state for this group and stream.
           # If we have then use the stored forward_token to pick up
           # from that point. Otherwise start from the start.
@@ -118,22 +138,29 @@ module Fluent::Plugin
 
           begin
             loop do
-              response = @aws.get_log_events(log_group_name: group, log_stream_name: stream, next_token: stream_token)
+              response = @aws.get_log_events(
+                log_group_name: group,
+                log_stream_name: stream,
+                next_token: stream_token
+              )
+
               emit(response.events)
               break unless response.next_token
               stream_token = response.next_token
             end
 
-            # Once all events for this stream have been processed, store the forward token
+            # Once all events for this stream have been processed,
+            # store the forward token
             state[group][stream] = response.next_forward_token
           rescue => boom
-            log.error("Unable to retrieve events for stream #{stream} in group #{group}: #{boom}")
+            log.error("Unable to retrieve events for stream
+              #{stream} in group #{group}: #{boom}")
           end
         end
       end
 
       log.info('Pruning and saving state')
-      state.prune(log_groups) # Remove any log groups and associated streams that no longer exist
+      state.prune(log_groups) # Remove dead streams
       state.save
       log.info("Pausing for #{@interval}")
       sleep @interval
