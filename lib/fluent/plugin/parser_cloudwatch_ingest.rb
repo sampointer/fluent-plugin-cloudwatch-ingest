6b1fc07aef969f963ecbcd69484f8b7f3b2c042b
Sam Pointer <sam.pointer@opsunit.com>
2017-03-20 18:06:52 +0000

    lint

--- lib/fluent/plugin/parser_cloudwatch_ingest.rb
+++ lib/fluent/plugin/parser_cloudwatch_ingest.rb
@@ -4,25 +4,29 @@ require 'fluent/time'
 module Fluent
   module Plugin
     class CloudwatchIngestParser < RegexpParser
-      Plugin.register_parser("cloudwatch_ingest", self)
+      Plugin.register_parser('cloudwatch_ingest', self)
 
-      config_set_default :expression, %q{/(?<message>.+)}
+      config_set_default :expression, '/(?<message>.+)'
       config_set_default :time_format, '%Y-%m-%d %H:%M:%S.%L'
 
       def parse(event)
-        time, record = super(event.message) { |t,r| t, r }
+        time = nil
+        record = nil
+        super(event.message) do |t, r|
+          time = t
+          record = r
+        end
 
         # We do String processing on the event time here to
         # avoid rounding errors introduced by floating point
         # arithmetic.
         event_s  = event.timestamp.to_s[0..9]
-        event_ns = event.timestamp.to_s[10..-1].to_i * 1000000
-
+        event_ns = event.timestamp.to_s[10..-1].to_i * 1_000_000
 
         # If we cannot parse the time from the message itself (either because
         # a time field is not included in the format, or it is not matched)
         # then we take the time from the cloudwatch event time
-        time = time ? time : Fluent::EventTime.new(event_s, event_ms)
+        time = time ? time : Fluent::EventTime.new(event_s, event_ns)
         yield time, record
       end
     end
